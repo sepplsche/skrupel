2a07fa8862c6a404b6ef9b49c7b8b6a09bb193e0
seppl <seb.bayer@web.de>
2016-09-24 13:39:49 +0200

    flugberechnen in eigenen step ausgelagert

--- inhalt/inc.zugberechnen.step4.php
+++ inhalt/inc.zugberechnen.step4.php
@@ -1,178 +1,209 @@
 <?php
 
-///////////////////////////////////////////////////////////////////////////////////////////////STATS INITIALISIEREN ANFANG
+///////////////////////////////////////////////////////////////////////////////////////////////ROUTEBEAMEN ANFANG
+$zeiger = mysql_query("SELECT * FROM $skrupel_schiffe where status=2 and routing_status=2 and spiel=$spiel order by id");
+$schiffanzahl = mysql_num_rows($zeiger);
 
-$stat_sieg = array_fill(1, 10, 0);
-$stat_schlacht = array_fill(1, 10, 0);
-$stat_schlacht_sieg = array_fill(1, 10, 0);
-$stat_kol_erobert = array_fill(1, 10, 0);
-$stat_lichtjahre = array_fill(1, 10, 0);
-
-///////////////////////////////////////////////////////////////////////////////////////////////STATS INITIALISIEREN ENDE
-///////////////////////////////////////////////////////////////////////////////////////////////LETZTER MONAT ANFANG
-$zeiger = mysql_query("SELECT * FROM $skrupel_zugberechnen_daten WHERE sid='$sid'");
-$array = mysql_fetch_array($zeiger);
-
-$neuekolonie = $array['neuekolonie'];
-$neueschiffe = $array['neueschiffe'];
-$neuebasen = $array['neuebasen'];
-$schiffevernichtet = $array['schiffevernichtet'];
-$planetenerobert = $array['planetenerobert'];
-$planetenerobertfehl = $array['planetenerobertfehl'];
-$schiffverschollen = $array['schiffverschollen'];
-
-if ($neuekolonie==0) {$neuekolonie=$lang['host'][$language]['letztermonat'][0];}
-if ($neuekolonie==1) {$neuekolonie=$lang['host'][$language]['letztermonat'][1];}
-if ($neuekolonie>=2) {$neuekolonie=str_replace(array('{1}'),array($neuekolonie),$lang['host'][$language]['letztermonat'][2]);}
-
-if ($neueschiffe==0) {$neueschiffe=$lang['host'][$language]['letztermonat'][3];}
-if ($neueschiffe==1) {$neueschiffe=$lang['host'][$language]['letztermonat'][4];}
-if ($neueschiffe>=2) {$neueschiffe=str_replace(array('{1}'),array($neueschiffe),$lang['host'][$language]['letztermonat'][5]);}
-
-if ($neuebasen==0) {$neuebasen=$lang['host'][$language]['letztermonat'][6];}
-if ($neuebasen==1) {$neuebasen=$lang['host'][$language]['letztermonat'][7];}
-if ($neuebasen>=2) {$neuebasen=str_replace(array('{1}'),array($neuebasen),$lang['host'][$language]['letztermonat'][8]);}
-
-if ($schiffevernichtet==0) {$schiffevernichtet=$lang['host'][$language]['letztermonat'][9];}
-if ($schiffevernichtet==1) {$schiffevernichtet=$lang['host'][$language]['letztermonat'][10];}
-if ($schiffevernichtet>=2) {$schiffevernichtet=str_replace(array('{1}'),array($schiffevernichtet),$lang['host'][$language]['letztermonat'][11]);}
-
-if ($planetenerobert==0) {$planetenerobert=$lang['host'][$language]['letztermonat'][12];}
-if ($planetenerobert==1) {$planetenerobert=$lang['host'][$language]['letztermonat'][13];}
-if ($planetenerobert>=2) {$planetenerobert=str_replace(array('{1}'),array($planetenerobert),$lang['host'][$language]['letztermonat'][14]);}
-
-if ($planetenerobertfehl==0) {$planetenerobertfehl="";}
-if ($planetenerobertfehl==1) {$planetenerobertfehl=$lang['host'][$language]['letztermonat'][15];}
-if ($planetenerobertfehl>=2) {$planetenerobertfehl=str_replace(array('{1}'),array($planetenerobertfehl),$lang['host'][$language]['letztermonat'][16]);}
-
-if ($schiffverschollen==0) {$schiffverschollen=$lang['host'][$language]['letztermonat'][21];}
-if ($schiffverschollen==1) {$schiffverschollen=$lang['host'][$language]['letztermonat'][22];}
-if ($schiffverschollen>=2) {$schiffverschollen=str_replace(array('{1}'),array($schiffverschollen),$lang['host'][$language]['letztermonat'][23]);}
-
-
-$letztermonat=str_replace(array('{1}','{2}','{3}','{4}','{5}','{6}','{7}'),array($neuekolonie,$neueschiffe,$neuebasen,$schiffevernichtet,$planetenerobert,$planetenerobertfehl,$schiffverschollen),$lang['host'][$language]['letztermonat'][17]);
-
-$zeiger_temp = mysql_query("UPDATE $skrupel_spiele set letztermonat='$letztermonat', runde=runde+1 where id=$spiel;");
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////ZIEL ANFANG
-
-$endejetzt=0;
-
-if ($ziel_id==1) {
-    if ($spieleranzahl<=intval($ziel_info)) {
-        $endejetzt=1;
-    }
-}
-if ($ziel_id==2) {
-    if (($spieler_raus_c[1]==1) or ($spieler_raus_c[5]==1) or ($spieler_raus_c[8]==1) or ($spieler_raus_c[2]==1) or ($spieler_raus_c[6]==1) or ($spieler_raus_c[9]==1) or ($spieler_raus_c[3]==1) or ($spieler_raus_c[7]==1) or ($spieler_raus_c[10]==1) or ($spieler_raus_c[4]==1)) {
-        $endejetzt=1;
-    }
-}
-
-if ($ziel_id==5) {
-
-    for ($k=1;$k<11;$k++) {
-        $spieler_ziel_t_c[$k]=0;
-    }
-
-    $zeiger = mysql_query("SELECT status,spiel,id,besitzer,fracht_min3 FROM $skrupel_schiffe where status<>2 and spiel=$spiel order by id");
-    $schiffanzahl = mysql_num_rows($zeiger);
-
-    if ($schiffanzahl>=1) {
-        for ($i=0; $i<$schiffanzahl;$i++) {
-            $ok = mysql_data_seek($zeiger,$i);
-            $array = mysql_fetch_array($zeiger);
-            $besitzer=$array["besitzer"];
-            $fracht_min3=$array["fracht_min3"];
-
-            if ($fracht_min3>=1) { $spieler_ziel_t_c[$besitzer]=$spieler_ziel_t_c[$besitzer]+$fracht_min3; }
-        }
-    }
-
-    $zeiger_temp = mysql_query("UPDATE $skrupel_spiele set spieler_1_ziel='$spieler_ziel_t_c[1]',spieler_2_ziel='$spieler_ziel_t_c[2]',spieler_3_ziel='$spieler_ziel_t_c[3]',spieler_4_ziel='$spieler_ziel_t_c[4]',spieler_5_ziel='$spieler_ziel_t_c[5]',spieler_6_ziel='$spieler_ziel_t_c[6]',spieler_7_ziel='$spieler_ziel_t_c[7]',spieler_8_ziel='$spieler_ziel_t_c[8]',spieler_9_ziel='$spieler_ziel_t_c[9]',spieler_10_ziel='$spieler_ziel_t_c[10]' where id=$spiel;");
-    $temp=intval($ziel_info);
-
-    if (($spieler_ziel_t_c[1]>=$temp) or ($spieler_ziel_t_c[2]>=$temp) or ($spieler_ziel_t_c[3]>=$temp) or ($spieler_ziel_t_c[4]>=$temp) or ($spieler_ziel_t_c[5]>=$temp) or ($spieler_ziel_t_c[6]>=$temp) or ($spieler_ziel_t_c[7]>=$temp) or ($spieler_ziel_t_c[8]>=$temp) or ($spieler_ziel_t_c[9]>=$temp) or ($spieler_ziel_t_c[10]>=$temp)) {
-        $endejetzt=1;
-    }
-}
-
-if ($ziel_id==6) {
-    if (($spieler_raus_c[1]==1) or ($spieler_raus_c[5]==1) or ($spieler_raus_c[8]==1) or ($spieler_raus_c[2]==1) or ($spieler_raus_c[6]==1) or ($spieler_raus_c[9]==1) or ($spieler_raus_c[3]==1) or ($spieler_raus_c[7]==1) or ($spieler_raus_c[10]==1) or ($spieler_raus_c[4]==1)) {
-        $endejetzt=1;
-    }
-}
-
-
-if ($endejetzt==1) {
-    include(INCLUDEDIR.'inc.host_spielende.php');
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////ZIEL ENDE
-///////////////////////////////////////////////////////////////////////////////////////////////STATS AUSWERTUNG ANFANG
-
-for ($m=1;$m<11;$m++) {
-    if ($spieler_id_c[$m]>=1) {$zeiger = mysql_query("UPDATE $skrupel_user set stat_sieg=stat_sieg+$stat_sieg[$m],stat_schlacht=stat_schlacht+$stat_schlacht[$m],stat_schlacht_sieg=stat_schlacht_sieg+$stat_schlacht_sieg[$m],stat_kol_erobert=stat_kol_erobert+$stat_kol_erobert[$m],stat_lichtjahre=stat_lichtjahre+$stat_lichtjahre[$m],stat_monate=stat_monate+1 where id=$spieler_id_c[$m]"); }
-}
-if ((@file_exists($xstats_verzeichnis)) and (intval(substr($spiel_extend,2,1))==1)) {
-    xstats_collectAndStore( $sid, &$stat_schlacht,&$stat_schlacht_sieg,&$stat_kol_erobert,&$stat_lichtjahre);
-}
-///////////////////////////////////////////////////////////////////////////////////////////////STATS AUSWERTUNG ENDE
-/////////////////////////////////////////////////////////////////////////////////////////////// BENACHRICHTIGUNG ANFANG
-
-
-for ($k=1; $k<=10; $k++) {
-    if ($spieler_id_c[$k]>0 and $spieler_raus_c[$k]==0) {
-        $nachrichtemail=str_replace('{1}',$spiel_name,$lang['host'][$spielersprache[$k]]['letztermonat'][18]);
-        $nachrichticq=str_replace('{1}',$spiel_name,$lang['host'][$spielersprache[$k]]['letztermonat'][24]);
-        $emailtopic=str_replace(array('{1}'),array($spiel_name),$lang['host'][$spielersprache[$k]]['letztermonat'][20]);
-
-        $zeiger = mysql_query("SELECT * FROM $skrupel_user WHERE id={$spieler_id_c[$k]}");
+if ($schiffanzahl>=1) {
+    for ($i=0; $i<$schiffanzahl;$i++) {
+        $ok = mysql_data_seek($zeiger,$i);
         $array = mysql_fetch_array($zeiger);
-        $emailadresse=$array['email'];
-        $icqnummer=$array['icq'];
-        $optionen=$array['optionen'];
-        $emailicq=$icqnummer."@pager.icq.com";
+        $shid=$array["id"];
+        $besitzer=$array["besitzer"];
+        $routing_id=$array["routing_id"];
+        $routing_tank=$array["routing_tank"];
+        $routing_schritt=$array["routing_schritt"];
+        $routing_mins=$array["routing_mins"];
+        $routing_rohstoff=(int)$array["routing_rohstoff"];
+        $routing_id_temp=explode(":",$routing_id);
+        $zid=$routing_id_temp[$routing_schritt];
+        $routing_mins_temp=explode(":",$routing_mins);
+        $mins=$routing_mins_temp[$routing_schritt];
+
+        $r_option[0]=(int)substr($mins,0,1);
+        $r_option[1]=(int)substr($mins,1,1);
+        $r_option[2]=(int)substr($mins,3,1);
+        $r_option[3]=(int)substr($mins,4,1);
+        $r_option[4]=(int)substr($mins,5,1);
+        $r_option[5]=(int)substr($mins,7,7);
+        $r_option[6]=(int)substr($mins,14,4);
+        $r_option[7]=(int)substr($mins,18,4);
+
+        $mins_lemin=substr($mins,2,1);
+        $voll_laden=substr($mins,6,1);
+
+        $r_fracht[0]=(int)$array["fracht_cantox"];
+        $r_fracht[1]=(int)$array["fracht_vorrat"];
+        $r_fracht[2]=(int)$array["fracht_min1"];
+        $r_fracht[3]=(int)$array["fracht_min2"];
+        $r_fracht[4]=(int)$array["fracht_min3"];
+        $r_fracht[5]=(int)$array["fracht_leute"];
+        $r_fracht[6]=(int)$array["leichtebt"];
+        $r_fracht[7]=(int)$array["schwerebt"];
+
+        $fracht_lemin=$array["lemin"];
+        $frachtraum=$array["frachtraum"];
+        $leminmax=$array["leminmax"];
+
+        $kox=$array["kox"];
+        $koy=$array["koy"];
+
+        $r_faktor[0]=0; //cantox
+        $r_faktor[1]=100; //vorrat
+        $r_faktor[2]=100; //bax
+        $r_faktor[3]=100; //ren
+        $r_faktor[4]=100; //vor
+        $r_faktor[5]=1; //kol
+        $r_faktor[6]=30;  //lbt
+        $r_faktor[7]=150; //sbt
+
+        $freiraum=$frachtraum*100;
+        $freitank=$leminmax-$fracht_lemin;
+
+        for($zaehler=0;$zaehler<8;$zaehler++){
+            $freiraum-=$r_fracht[$zaehler]*$r_faktor[$zaehler];
+        }
 
 
-        $url="http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'];
-        $url=substr($url,0,strlen($url)-19);
-        
-        $url="http://".$_SERVER['SERVER_NAME'];
-        $folders = explode('/', $_SERVER['SCRIPT_NAME']);
-        $count = 0;
-        $url .= '/';
-        foreach ($folders as $value) {
-            if ((0 < $count) and (count($folders) > $count+1) and ('inhalt' != $value)){
-                $url .= $value . '/';
+
+        $zeiger2 = mysql_query("SELECT * FROM $skrupel_planeten where x_pos=$kox and y_pos=$koy and besitzer=$besitzer and id=$zid and spiel=$spiel");
+        $planetenanzahl = mysql_num_rows($zeiger2);
+
+        if ($planetenanzahl==1) {
+            $array2 = mysql_fetch_array($zeiger2);
+            $p_id=$array2["id"];
+            $p_lemin=$array2["lemin"];
+
+            $r_planet[0]=(int)$array2["cantox"];
+            $r_planet[1]=(int)$array2["vorrat"];
+            $r_planet[2]=(int)$array2["min1"];
+            $r_planet[3]=(int)$array2["min2"];
+            $r_planet[4]=(int)$array2["min3"];
+            $r_planet[5]=(int)$array2["kolonisten"];
+            $r_planet[6]=(int)$array2["leichtebt"];
+            $r_planet[7]=(int)$array2["schwerebt"];
+
+
+            //ausladen
+            for($zaehler=0;$zaehler<8;$zaehler++){
+                if ($r_option[$zaehler]==2) {
+                    $r_planet[$zaehler]+=$r_fracht[$zaehler];
+                    $freiraum+=$r_fracht[$zaehler]*$r_faktor[$zaehler];
+                    $r_fracht[$zaehler]=0;
+                }
             }
-            $count++;
-        }        
-        $hash=$spieler_hash[$k];
 
-        $nachricht_fertig = $nachrichtemail."\n\n".$url.'index.php?hash='.$hash;
+            //ausladen relativ
+            for($zaehler=5;$zaehler<8;$zaehler++){
+                if (($r_option[$zaehler]>2)and($r_option[$zaehler]>$r_planet[$zaehler])){
+                    $zwischen=min($r_option[$zaehler]-$r_planet[$zaehler],$r_fracht[$zaehler]);
+                    $r_planet[$zaehler]+=$zwischen;
+                    $r_fracht[$zaehler]-=$zwischen;
+                    $freiraum+=$zwischen*$r_faktor[$zaehler];
+                }
+            }
 
-        if (substr($optionen,0,1)=='1') {
-        @mail($emailadresse,$emailtopic, $nachricht_fertig,
-            "From: $absenderemail\r\n"
-            ."Reply-To: $absenderemail\r\n"
-            ."X-Mailer: PHP/" . phpversion());
+            //einladen wichtigstes Gut
+            if ($r_option[$routing_rohstoff]==1){
+                if (($r_planet[$routing_rohstoff]*$r_faktor[$routing_rohstoff])<=$freiraum) {
+                    $freiraum=$freiraum-($r_planet[$routing_rohstoff]*$r_faktor[$routing_rohstoff]);
+                    $r_fracht[$routing_rohstoff]+=$r_planet[$routing_rohstoff];
+                    $r_planet[$routing_rohstoff]=0;
+                }else{
+                    $r_planet[$routing_rohstoff]=$r_planet[$routing_rohstoff]-(int)floor($freiraum/$r_faktor[$routing_rohstoff]);
+                    $r_fracht[$routing_rohstoff]+=(int)floor($freiraum/$r_faktor[$routing_rohstoff]);
+                    $freiraum-=(int)floor($freiraum/$r_faktor[$routing_rohstoff])*$r_faktor[$routing_rohstoff];
+                }
+            }elseif(($r_option[$routing_rohstoff]>2)and($r_option[$routing_rohstoff]<$r_planet[$routing_rohstoff])){
+                    $zwischen=min($r_planet[$routing_rohstoff]-$r_option[$routing_rohstoff],(int)floor($freiraum/$r_faktor[$routing_rohstoff]));
+                    $r_planet[$routing_rohstoff]-=$zwischen;
+                    $r_fracht[$routing_rohstoff]+=$zwischen;
+                    $freiraum-=$zwischen*$r_faktor[$routing_rohstoff];
+            }
+
+            //einladen relativ
+            for($zaehler=5;$zaehler<8;$zaehler++){
+                if (($r_option[$zaehler]>2)and($r_option[$zaehler]<$r_planet[$zaehler])){
+                    $zwischen=min($r_planet[$zaehler]-$r_option[$zaehler],(int)floor($freiraum/$r_faktor[$zaehler]));
+                    $r_planet[$zaehler]-=$zwischen;
+                    $r_fracht[$zaehler]+=$zwischen;
+                    $freiraum-=$zwischen*$r_faktor[$zaehler];
+                }
+            }
+
+            //einladen cantox(da sonst divison durch null)
+            if ($r_option[0]==1){
+                $r_fracht[0]+=$r_planet[0];
+                $r_planet[0]=0;
+            }
+
+            //einladen(Vorr�te zum schlu�)
+            $ztest=1;
+            $zaehler=2;
+            while($ztest==1){
+                if($zaehler==1){$ztest=0;}
+                if ($r_option[$zaehler]==1){
+                    if (($r_planet[$zaehler]*$r_faktor[$zaehler])<=$freiraum) {
+                        $freiraum=$freiraum-($r_planet[$zaehler]*$r_faktor[$zaehler]);
+                        $r_fracht[$zaehler]+=$r_planet[$zaehler];
+                        $r_planet[$zaehler]=0;
+                    }else{
+                        $r_planet[$zaehler]=$r_planet[$zaehler]-(int)floor($freiraum/$r_faktor[$zaehler]);
+                        $r_fracht[$zaehler]+=(int)floor($freiraum/$r_faktor[$zaehler]);
+                        $freiraum-=(int)floor($freiraum/$r_faktor[$zaehler])*$r_faktor[$zaehler];
+                    }
+                }
+                $zaehler=($zaehler==7)?1:$zaehler+1;
+            }
+
+            //rest
+            if ($mins_lemin==1) {
+                if ($p_lemin<=$freitank) {
+                    $freitank=$freitank-$p_lemin;
+                    $fracht_lemin=$fracht_lemin+$p_lemin;
+                    $p_lemin=0;
+                }else{
+                    $p_lemin=$p_lemin-$freitank;
+                    $fracht_lemin=$fracht_lemin+$freitank;
+                    $freitank=0;
+                }
+            }
+            if ($mins_lemin==2) {
+                $p_lemin=$p_lemin+$fracht_lemin;
+                $fracht_lemin=0;
+            }
+
+            if (($fracht_lemin<$routing_tank) and ($p_lemin>0)) {
+                $fehlt=$routing_tank-$fracht_lemin;
+                if ($fehlt<=$p_lemin) {
+                    $p_lemin=$p_lemin-$fehlt;$fracht_lemin=$routing_tank;
+                }else{
+                    $fracht_lemin=$fracht_lemin+$p_lemin;$p_lemin=0;
+                }
+            }
+
+            $s_cantox=$r_planet[0];
+            $s_vorrat=$r_planet[1];
+            $s_bax=$r_planet[2];
+            $s_ren=$r_planet[3];
+            $s_vor=$r_planet[4];
+            $s_kol=$r_planet[5];
+            $s_lbt=$r_planet[6];
+            $s_sbt=$r_planet[7];
+            $zeigertemp = mysql_query("UPDATE $skrupel_planeten set lemin=$p_lemin,cantox=$s_cantox,vorrat=$s_vorrat,min1=$s_bax,min2=$s_ren,min3=$s_vor,kolonisten=$s_kol,leichtebt=$s_lbt,schwerebt=$s_sbt where id=$p_id");
+
+            $s_cantox=$r_fracht[0];
+            $s_vorrat=$r_fracht[1];
+            $s_bax=$r_fracht[2];
+            $s_ren=$r_fracht[3];
+            $s_vor=$r_fracht[4];
+            $s_kol=$r_fracht[5];
+            $s_lbt=$r_fracht[6];
+            $s_sbt=$r_fracht[7];
+            $zeigertemp = mysql_query("UPDATE $skrupel_schiffe set fracht_leute=$s_kol,leichtebt=$s_lbt,schwerebt=$s_sbt,lemin=$fracht_lemin,fracht_vorrat=$s_vorrat,fracht_cantox=$s_cantox,fracht_min1=$s_bax,fracht_min2=$s_ren,fracht_min3=$s_vor where id=$shid");
         }
-        /*
-        if (substr($optionen,1,1)=='1') {
-            $header="From $absenderemail\nReply-To:$absenderemail\n";
-            @mail($emailicq,$emailtopic,"$nachrichticq",$header);
-        }
-        */
     }
 }
-$nachricht=str_replace('{1}',$spiel_name,$lang['host'][$language]['letztermonat'][19]);
-$aktuell=time();
-$zeiger = mysql_query("INSERT INTO $skrupel_chat (spiel,datum,text,an,von,farbe) values ($spiel,'$aktuell','$nachricht',0,'System','000000');");
+///////////////////////////////////////////////////////////////////////////////////////////////ROUTEBEAMEN ENDE
 
-
-/////////////////////////////////////////////////////////////////////////////////////////////// BENACHRICHTIGUNG ENDE
-
-///////////////////////////////////////////////////////////////////////////////////////////////LETZTER MONAT ENDE
 ?>
